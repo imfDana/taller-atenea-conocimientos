a6a02029771a9412621aea26b2e3912321ee4c50
Dana <danaimfeld@gmail.com>
2026-01-15 21:08:07 -0300

    Modificacion a las primeras pruebas de Registro

--- tests/registro.spec.ts
+++ tests/registro.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('TC-01 Verificar elementos visuales en la página de registro', async ({ page }) => {
+test('TC-01 Visiblidad de los campos iniciales', async ({ page }) => {
     await page.goto('http://localhost:3000/');
     await expect(page.locator('input[name="firstName"]')).toBeVisible();
     await expect(page.locator('input[name="lastName"]')).toBeVisible();
@@ -10,13 +10,13 @@ test('TC-01 Verificar elementos visuales en la página de registro', async ({ pa
     // await page.waitForTimeout(5000);
 });
 
-test('TC-02 Verificar que el boton de registrarse est deshabilitado por default', async ({ page }) => {
+test('TC-02 Boton de Registrarse deshabilitado por defecto', async ({ page }) => {
 
     await page.goto('http://localhost:3000/');
     await expect(page.getByTestId('boton-registrarse')).toBeDisabled();
 });
 
-test('TC- 03 Verificar se habilite al completar los campos obligatorios', async ({ page }) => {
+test('TC- 03 Habilitacion del boton con el completado de los campos obligatorios', async ({ page }) => {
 
     await page.goto('http://localhost:3000');
     await page.locator('input[name="firstName"]').fill('Dana');
@@ -27,13 +27,13 @@ test('TC- 03 Verificar se habilite al completar los campos obligatorios', async 
 
 });
 
-test('TC-04 Verificar redireccionamento a la pagina de inicio al hacer click en Registrarse', async ({ page }) => {
+test('TC-04 Redireccion al Login desde el header', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.getByTestId('boton-login-header-signup').click();
     await expect(page).toHaveURL('http://localhost:3000/login');
 });
 
-test('TC-05 Verificar registro exitoso con datos validos', async ({ page }) => {
+test('TC-05 Registro exitoso de nuevo usuario', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.locator('input[name="firstName"]').fill('Dana');
     await page.locator('input[name="lastName"]').fill('Imfeld');
@@ -44,18 +44,7 @@ test('TC-05 Verificar registro exitoso con datos validos', async ({ page }) => {
 
 });
 
-test('TC-06 Verificar registro exitoso con datos validos', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.locator('input[name="firstName"]').fill('Dana');
-    await page.locator('input[name="lastName"]').fill('Imfeld');
-    await page.locator('input[name="email"]').fill('pepeargento'+Date.now().toString()+'@email.com')
-    await page.locator('input[name="password"]').fill('Curs0deTesting');
-    await page.getByTestId('boton-registrarse').click();
-    await expect(page.getByText('Registro exitoso')).toBeVisible();
-
-});
-
-test('TC-07 Verificar mensaje de error ante correo existente', async ({ page }) => {
+test('TC-06 Mensaje de error por email duplicado', async ({ page }) => {
     const email = 'pepeargento'+ Date.now().toString() +'@email.com';
     await page.goto('http://localhost:3000');
     await page.locator('input[name="firstName"]').fill('Dana');
